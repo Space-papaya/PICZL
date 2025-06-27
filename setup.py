64979154c2063c8ec9759b31f4e14e749ffaf45f
Raphael Shirley <rshirley@mpe.mpg.de>
2025-06-27 14:31:51 +0200

    Added setup.py
    We need to start a minimal package

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="piczl",  # Replace with your package name
+    version="0.1.0",
+    author="William Roster",
+    author_email="wroster@mpe.mpg.de",
+    description="PICZL photometric redshifts.",
+    packages=find_packages(where="src"),  # Specify 'src' as the source folder
+    package_dir={"": "src"},  # Specify 'src' as the package root
+    install_requires=[  # List your package dependencies here
+        "tensorflow",
+        "torch",
+        "pandas",
+        "matplotlib",
+        "scikit-learn",
+        "PyYAML",
+        "astropy",
+    ],
+    python_requires=">=3.6",  # Specify the minimum Python version
+)
