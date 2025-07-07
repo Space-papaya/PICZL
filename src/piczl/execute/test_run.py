b2705781341fc72b51be7f0254b04cca467ec7e6
Raphael Shirley <rshirley@mpe.mpg.de>
2025-07-07 17:49:21 +0200

    moved tests aND deleted old tests

--- src/piczl/execute/test_run.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
-from piczl.execute.run import predict_redshifts
-
-def test_run_function():
-    predict_redshifts()
--- tests/piczl/test_run.py
+++ tests/piczl/test_run.py
@@ -1,25 +1,9 @@
-import os
-import tensorflow as tf
 import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'src')))
-from piczl.core.estimator import run_estimation
-from piczl.utilities import *
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
-test_dir = os.path.abspath(os.path.dirname(__file__))
-DATA_PATH = os.path.join(test_dir, "../example_data/small/")
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+from piczl.execute.run import predict_redshifts
 
-
-with tf.device('/GPU:0'):
-	z_modes, l1s, u1s, degeneracy, dataset = run_estimation(
-	catalog_path=DATA_PATH + "example_cat.fits",
-	image_path=DATA_PATH,
-	mode='inactive',
-	sub_sample = False,
-	max_sources=20
-	)
-
-print("z_peak:", z_modes[:5])
-print("l1s:", l1s[:5])
-print("u1s:", u1s[:5])
-print("degeneracy:", degeneracy[:5])
-
+def test_run_function():
+    predict_redshifts()
