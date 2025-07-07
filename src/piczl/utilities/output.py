e10dde6890c74ffbe10ecd99e651b58273fbee16
Space-papaya <william@rosters.de>
2025-07-07 17:11:31 +0200

    Saving test results sorted

--- src/piczl/utilities/output.py
+++ src/piczl/utilities/output.py
@@ -9,8 +9,7 @@ def append_output(dataset, pwd, catalog_name, ens_modes, l1s, u1s, degeneracy):
 	dataset['phz_inact'] = ens_modes
 	dataset['lower_1sig_inact'] = l1s
 	dataset['upper_1sig_inact'] = u1s
-	#dataset['pdf_in_inverval'] = areas
 	dataset['pdf_degenercy'] = degeneracy
 
 	tab = Table.from_pandas(dataset)
-	tab.write(pwd + catalog_name + 'piczl.fits', overwrite=True)
+	tab.write(pwd + catalog_name + 'demo_catalog.fits', overwrite=True)
