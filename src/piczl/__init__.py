688d7303d5e22df9cf37cf48defb944f73909aa8
Raphael Shirley <rshirley@mpe.mpg.de>
2025-07-09 11:10:52 +0200

    Fix test issue
    there was an init file in the tests directory

--- src/piczl/__init__.py
+++ src/piczl/__init__.py
@@ -1,15 +1,6 @@
-import os
-import pkgutil
-import importlib
-
-# Get the directory of this __init__.py file
-__all__ = []
-
-package_dir = os.path.dirname(__file__)
-
-# Iterate through all modules and subpackages in this package
-for _, module_name, is_pkg in pkgutil.iter_modules([package_dir]):
-    if not module_name.startswith("_"):
-        module = importlib.import_module(f".{module_name}", package=__name__)
-        globals()[module_name] = module
-        __all__.append(module_name)
+from . import config
+from . import core
+from . import demo_data
+from . import execute
+from . import models
+from . import utilities
