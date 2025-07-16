0fb4867556cca81d115354c8e9a88ff08a1c1903
William Roster <83342142+williamroster@users.noreply.github.com>
2025-07-16 11:03:43 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,6 +23,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',  # Optional, for Google or NumPy docstrings
     'sphinx.ext.viewcode',  # Optional, adds links to source code
+    'sphinx_book_theme'
 ]
 
 templates_path = ['_templates']
@@ -33,5 +34,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme' #sphinx_rtd_theme
 html_static_path = ['_static']
